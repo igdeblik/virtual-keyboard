462a3f8fe4dd74837d55be09e5dd2f9e2758852b
igdeblik <igdeblik94@gmail.com>
2023-05-01 22:16:38 +0300

    refactor: rename module

--- components/keyboard/keyUp.js
+++
@@ -1,9 +0,0 @@
-export const creatKeyCap = (key) => {
-    const defaultSize = 50;
-    const keyCap = document.createElement('div');
-    keyCap.className = 'keyboard-btn';
-    keyCap.textContent = key.en[0];
-    keyCap.style.width = `${defaultSize * (key.size || 1)}px`;
-  
-    return keyCap;
-  }
