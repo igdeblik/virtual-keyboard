c8ece92efd6ec384823d237262f591b0c01dec69
igdeblik <igdeblik94@gmail.com>
2023-05-01 22:18:18 +0300

    fix: fix text function

--- components/textarea/index.js
+++ components/textarea/index.js
@@ -1,5 +1,19 @@
+export const textareaId = 'textarea';
+
+export const updateTextarea = (e) => {
+  const textarea = document.getElementById(textareaId);
+  let value = textarea.value;
+  if(e.code === 'Backspace') {
+    value = value.slice(0, -1);
+  } else {
+   value += e.key;
+}
+textarea.value = value;
+}
+
 export const createTextArea = () => {
    const textarea =  document.createElement('textarea');
   textarea.className = 'textstring';
+  textarea.id = textareaId;
   return textarea;
 }
