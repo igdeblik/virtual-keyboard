a0991887ccc323a0fc8c2950d5b0a5eb4af6161b
igdeblik <igdeblik94@gmail.com>
2023-05-01 22:15:28 +0300

    fix: create keyboard

--- components/keyboard/index.js
+++ components/keyboard/index.js
@@ -1,10 +1,50 @@
-import { creatKeyCap } from "./keyUp.js";
+import {creatKeyCap} from '../../components/keyboard/keyCap.js'
+import {updateTextarea} from '../textarea/index.js'
+import { getOptions } from '../../service/getOptions.js';
+import keys from '../../data/keys.js';
 
-export const createKeyBoard = (data, container) => {
-  Object.entries(data).forEach(([key, value]) => {
+const handleVirtualKeyboardClick = (container) => {
+
+  container.addEventListener('mousedown', (e) => {
+      const el = e.target;
+
+      if(el.classList.contains('keyboard-btn')) {
+          el.classList.add('active');
+          updateTextarea(el.textContent);
+      }
+  })
+  container.addEventListener('mouseup', (e) => {
+      const el = e.target;
+
+      if(el.classList.contains('keyboard-btn')) {
+          el.classList.remove('active');
+      }
+  })
+}
+
+export const createKeyBoard = () => {
+  const keyboard = document.createElement('div');
+  const keyboardOptions = getOptions();
+  keyboard.className = 'keyboard';
+  keyboard.id = 'keyboard';
+
+  Object.entries(keys).forEach(([key, value]) => {
     const line = document.createElement('div');
     line.className = `line ${key}`;
-    line.append(...Object.values(value).map(key => creatKeyCap(key)))
-    container.append(line)
+    line.append(...value.map(key => creatKeyCap(key, keyboardOptions)))
+    keyboard.append(line);
   });
+
+  handleVirtualKeyboardClick(keyboard);
+
+  return keyboard;
 }
+
+
+const updateKeyBoard = () => {
+  document.getElementById('keyboard').remove();
+
+  document.getElementById('container').append(createKeyBoard());
+}
+
+document.addEventListener('newSettings', updateKeyBoard);
